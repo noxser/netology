2a0f3a300168432141c3de96cdc38bfd9cddf286
noxser <noxser@gmail.com>
2017-07-03 09:48:45 +0500

    Внес корректировки. По замечаниям

--- 2.4 HomeWork/find_procedure.py
+++ 2.4 HomeWork/find_procedure.py
@@ -24,32 +24,33 @@
 import os.path
 
 def find_migrations():
-    # генерируем абсолютный путь до папки Migrations
+    ''' Генерируем абсолютный путь до папки Migrations '''
     tree = os.walk(os.getcwd())
     for d in tree:
         if d[0].find('Migrations') >= 0:
             # можно .count() > 0
-            Migration_abs_path = d[0]
-    return Migration_abs_path
+            migration_abs_path = d[0]
+    return migration_abs_path
 
-def sort_for_extension(Migration_abs_path):
-    # находим файлы с расширение sql в папке migration с помощью path.splitext
-    files = os.listdir(Migration_abs_path)
+def sort_for_extension():
+    ''' Находим файлы с расширение sql в папке migration с помощью path.splitext '''
+    migration_abs_path = os.path.abspath(os.path.dirname(__file__))
+    files = os.listdir(migration_abs_path)
     new_list = []
     for file in files:
         name, extension = os.path.splitext(file)
         if extension == '.sql':
             new_list.append(file)
-    return new_list
+    return new_list, migration_abs_path
 
-def loop(new_list, Migration_abs_path):
+def loop(new_list, migration_abs_path):
     comand = input('Введите ключевое слово или значение\nq для завершения\n')
     if comand == 'q':
         exit()
     sort_list = []
-    path, dir_name = os.path.split(Migration_abs_path)
+    path, dir_name = os.path.split(migration_abs_path)
     for new_file in new_list:
-        with open((os.path.join(Migration_abs_path, new_file)), encoding='utf8') as f:
+        with open((os.path.join(migration_abs_path, new_file)), encoding='utf8') as f:
             s = f.read()
             if comand in s:
                 sort_list.append(new_file)
@@ -58,12 +59,11 @@ def loop(new_list, Migration_abs_path):
     # используем рекурсию для вызова функции саму собой
     # передаем ей отсортированный список в предедущей итерации и
     # так далее выход по q
-    return loop(sort_list, Migration_abs_path)
+    return loop(sort_list, migration_abs_path)
 
 def my_main():
-    Migration_abs_path = find_migrations()
-    new_list = sort_for_extension(Migration_abs_path)
-    loop(new_list, Migration_abs_path)
-
+    # migration_abs_path = find_migrations()
+    new_list, migration_abs_path = sort_for_extension()
+    loop(new_list, migration_abs_path)
 
 my_main()
