0534545648608191d199a69350e767de106281ff
noxser <noxser@gmail.com>
2017-08-30 22:33:19 +0500

    Чистый вариант для проверки

--- Diplom/graduet work_v_1_1_clear.py
+++ Diplom/graduet work_v_1_1_clear.py
@@ -2,22 +2,28 @@ from time import sleep, ctime
 import requests
 import json
 
+#  для проверок
+#  5030613 из заданимя 835 друга 122 группы
+#  4556271 мой 72 друга 56 групп
 
 TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
 VERSION = '5.68'
 params = {'access_token': TOKEN, 'v': VERSION}
 
 
-# только список друзей возврашает ID пользователей
-# для ускорения френдлист режу на 10
 def friends_list(params, user_id):
+    """
+    только список друзей возврашает ID пользователей
+    """
     params['user_id'] = user_id
     response = requests.get('https://api.vk.com/method/friends.get', params)
     return (response.json())['response']['items']
 
 
-# формирует ответ от groups.get
 def user_groups(params, user_id):
+    """
+    формирует ответ от groups.get
+    """
     params['count'] = '1000'
     params['extended'] = '0'
     params['user_id'] = user_id
@@ -25,63 +31,68 @@ def user_groups(params, user_id):
     return response.json()
 
 
-# запрашиваем группы внешней функцией и ищем уникальные
 def personal_group(params, friends_list, user_id_glob):
-    a = set(user_groups(params, user_id_glob)['response']['items'])
+    """
+    запрашиваем группы внешней функцией и ищем уникальные
+    группы которые есть только у юзера чей айди ввели в начале
+    """
+    us_gr = set(user_groups(params, user_id_glob)['response']['items'])
     i = 0
     for user in friends_list:
-        sleep(1)
+        sleep(0.35)
         i += 1
         gr = user_groups(params, user)  # список групп друга
         if gr.get('error', 'active') == 'active':  # делаем проверку на error
-            b = set(gr['response']['items'])  # преобразуем в множество
+            fr_gr = set(gr['response']['items'])  # преобразуем во множество
         # находим уникальные группы из 2-х множеств которые принадлежат только 1
-            a = a - b
+            us_gr = us_gr - fr_gr
         # нарыл клевую штуку для печати процесса работы )
         print('\rПроверенно друзей {} из {}'.format(i, len(friends_list)), end='', flush=True)
-    return a
+    return us_gr
 
 
-# получаем информацию о группах и тут уже формируем для выгрузки в json
 def group_info(params, group_ids):
+    """
+    получаем информацию о группах и тут уже формируем для выгрузки в json
+    """
     params['group_ids'] = ','.join(map(str, group_ids))
     params['fields'] = 'members_count'
     response = requests.get('https://api.vk.com/method/groups.getById', params)
     gr_lst = []
     for gr in (response.json())['response']:
         if gr.get('deactivated', 'active') == 'active':
-            gr_dict = {}
-            gr_dict['name'] = gr['name']
-            gr_dict['gid'] = gr['id']
-            gr_dict['members_count'] = gr['members_count']
+            gr_dict = {'name': gr['name'], 'gid': gr['id'], 'members_count': gr['members_count']}
             gr_lst.append(gr_dict)
     return gr_lst
 
 
-# формируем json
 def write_json(data):
+    """
+    формируем json
+    """
     with open('groups.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=1, ensure_ascii=0)
 
 
 # собираем все в кучу
-def main_script():
+if __name__ == '__main__':
     user_id_glob = int(input('Введите ID для поиска '))
     print()
     print('Начало работы скрипта', ctime())
     print('----------------------------------------------')
     fr_lst = friends_list(params, user_id_glob)
-    print('Список друзей сформирован')
-    print('Друзей', len(fr_lst))
+    us_gr = user_groups(params, user_id_glob)['response']['items']
+    print('Список друзей и групп пользователя сформирован')
+    print('Друзей {}\nГрупп {}'.format(len(fr_lst), len(us_gr)))
     print('----------------------------------------------')
-    print('Идет шпионаж')
+    print('Идет шпионаж, сидите тихо')
     sleep(1)
     a = (personal_group(params, fr_lst, user_id_glob))
     print()
     print('----------------------------------------------')
     sleep(1)
     print('У пользователя уникальных групп {} из {}'
-          .format(len(a), len(user_groups(params, user_id_glob)['response']['items']))
+          .format(len(a), len(us_gr))
           )
     sleep(1)
     write_json(group_info(params, a))
@@ -89,8 +100,4 @@ def main_script():
     print('Список групп находиться в файле groups.json')
     print('----------------------------------------------')
     print('Конец работы скрипта', ctime())
-
-#  id = '5030613'
-
-main_script()
 
