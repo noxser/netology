d4645f1bc36b453df5c5b5bd8487cbb61a27e8bd
noxser <noxser@gmail.com>
2017-09-12 22:47:39 +0500

    1. Добавил фун-ю для проверки введеного id
    2. Вынес в отдельную фун-ю запрос и формирование параметров к серверу
    
    3. Декомпозировал код...

--- Diplom/graduet work_v_1_2_clear.py
+++ Diplom/graduet work_v_1_2_clear.py
@@ -2,11 +2,8 @@ from time import sleep, ctime
 import requests
 import json
 
-#  для проверок
-#  5030613 и tim_leary из заданимя 835 друга 122 группы
-#  4556271 мой 72 друга 56 групп
 
-TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
+TOKEN = ''
 VERSION = '5.68'
 
 
